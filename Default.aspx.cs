0d87c85d8a652791c0e7ecc37d8712bd367f57d3
chmodder <hotmailgateway@gmail.com>
2013-10-08 01:17:04 +0200

    Started CB for random gallery (default.aspx)
    CB = code behind

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -12,5 +12,41 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //int i = 1;
+        //while (i <= 8)
+        //{
+        //    Response.Write("linje nr. " + i + "<br />");
+        //    i++;
+        //}
+        //if (!IsPostBack)
+        //{
+        //    // opret forbindelse til databasen
+        //    SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+        //    SqlCommand cmd = new SqlCommand();
+        //    cmd.Connection = conn;
+
+            
+        //    // SQL strengen
+        //    cmd.CommandText = "SELECT [Id], [navn] FROM Billeder WHERE Id = @random";
+        //    string random = ;
+        //    //ben for forbindelsen til databasen
+        //    conn.Open();
+
+        //    SqlDataReader reader = cmd.ExecuteReader();
+
+        //    if (reader.Read())
+        //    {
+        //        redigerOverskrift.Text = reader["overskrift"].ToString();
+        //        redigerManchet.Text = reader["manchet"].ToString();
+        //        redigerTekst.Text = reader["tekst"].ToString();
+        //        redigerForfatter.Text = reader["forfatter"].ToString();
+        //        redigerImg.Text = reader["img"].ToString();
+        //    }
+
+
+        //    // Luk for forbindelsen til databasen
+        //    // reader.Close();
+        //    conn.Close();
+        //}
     }
 }
